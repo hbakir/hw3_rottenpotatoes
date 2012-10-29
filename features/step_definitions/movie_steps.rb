45d5a1cc1a5e116c0a67797ef850d392c868911e
hbakir <hicham@bakir.ch>
2012-10-29 07:07:09 +0100

    Added the sorting steps definition.

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -14,7 +14,8 @@ end
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   #  ensure that that e1 occurs before e2.
   #  page.content  is the entire content of the page as a string.
-  flunk "Unimplemented"
+  regexp = /#{e1}.*#{e2}/m
+  page.body.should =~ regexp
 end
 
 # Make it easier to express checking or unchecking several boxes at once
